8345ea34fc34243ee4c25d14a25aec79a6127d4e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-10-21 17:57:57 +0200

    Register user fields required added.

--- api/DTOs/RegisterDto.cs
+++ api/DTOs/RegisterDto.cs
@@ -1,7 +1,11 @@
-﻿namespace API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
 
 public class RegisterDto
 {
+    [Required]
     public string UserName { get; set; }
+    [Required]
     public string Password { get; set; }
 }

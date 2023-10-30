752974510dbd43a60a705980dbd326de3ee995a4
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-10-30 17:09:42 +0100

    Home page

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+}

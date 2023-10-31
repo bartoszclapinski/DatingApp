b639b13e05b8216c71aaf4361dcdb48375c9d93f
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-10-31 15:12:01 +0100

    New components added - members details and list, messages and lists

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {MemberListComponent} from "./members/member-list/member-list.component";
+import {MessagesComponent} from "./messages/messages.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "members", component: MemberListComponent},
+  {path: "members/:id", component: MemberListComponent},
+  {path: "lists", component: MemberListComponent},
+  {path: "messages", component: MessagesComponent},
+  {path: "**", component: HomeComponent, pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

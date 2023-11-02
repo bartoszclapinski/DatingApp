51efb80e086b38a471e1bfdff13dea658dda725e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-02 16:15:36 +0100

    New theme from bootswatch added

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,7 +20,7 @@ export class AccountService {
           const user = response;
           if(user) {
             localStorage.setItem("user", JSON.stringify(user));
-            this.currentUserSource.next(user);
+            this.setCurrentUser(user);
           }
         })
       );

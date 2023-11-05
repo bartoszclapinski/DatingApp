1a2beba845a63ddf5de21d6b744b8ee78c9323b4
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-05 15:42:54 +0100

    Error controller added

---
+++ api/Controllers/BuggyController.cs
@@ -0,0 +1,44 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    private readonly AppDbContext _context;
+
+    public BuggyController(AppDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+    
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetSecret()
+    {
+        return "secret text";
+    }
+    
+    [HttpGet("not-found")]
+    public async Task<ActionResult<string>> GetNotFound()
+    {
+        var thing = await _context.Users.FindAsync(new Guid());
+        if (thing == null) return NotFound();
+        return Ok(thing);
+    }
+    
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError()
+    {
+        var thing = _context.Users.Find(new Guid());
+        var thingToReturn = thing.ToString();
+        return thingToReturn;
+    }
+    
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+}

6182598306f2fd11d6a214019809ed73c86d328e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-08 18:34:34 +0100

    Server error page added.

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-server-error',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-error.component.css']
 })
 export class ServerErrorComponent {
+  error: any;
+    constructor(private router: Router) {
+      const navigation = this.router.getCurrentNavigation();
+      this.error = navigation?.extras?.state?.["error"];
+    }
 
 }

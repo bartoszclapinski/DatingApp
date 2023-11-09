51644b118941c1d72fe44dc8719672703e4c1bec
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-09 12:28:02 +0100

    DateTime extension to calculate age added

--- api/Extensions/DateTimeExtensions.cs
+++ api/Extensions/DateTimeExtensions.cs
@@ -1,6 +1,12 @@
 ﻿namespace API.Extensions;
 
-public class DateTimeExtensions
+public static class DateTimeExtensions
 {
-    
+    public static int CalculateAge(this DateOnly dob)
+    {
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+        var age = today.Year - dob.Year;
+        if (dob > today.AddYears(-age)) age--;
+        return age;
+    }
 }

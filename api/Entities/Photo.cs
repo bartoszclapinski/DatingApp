032c437031113d1c7a12bad775778a2e34e40bcc
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-09 12:21:19 +0100

    User entity extended

---
+++ api/Entities/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class Photo
+{
+    public Guid Id { get; set; }
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string PublicId { get; set; }
+}

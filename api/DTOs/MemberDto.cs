2ab2945ea07865c8fb60ac0bd4d207e0ef10c1a7
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-13 15:57:36 +0100

    Member and photo dto

---
+++ api/DTOs/MemberDto.cs
@@ -0,0 +1,17 @@
+﻿namespace API.DTOs;
+
+public class MemberDto
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public int Age { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<PhotoDto> Photos { get; set; }
+}

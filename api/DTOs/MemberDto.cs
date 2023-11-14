1f8e19571b3cfd1f5174a4b3ff04f2dba991490d
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-14 13:53:02 +0100

    AutoMapper configuring

--- api/DTOs/MemberDto.cs
+++ api/DTOs/MemberDto.cs
@@ -13,5 +13,6 @@ public class MemberDto
     public string Interests { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
+    public string PhotoUrl { get; set; }
     public List<PhotoDto> Photos { get; set; }
 }
--- api/Helpers/AutoMapperProfiles.cs
+++ api/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,9 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<AppUser, MemberDto>();
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src =>
+                src.Photos.FirstOrDefault(x => x.IsMain).Url));
         CreateMap<Photo, PhotoDto>();
     }
 }

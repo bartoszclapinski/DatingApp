2e3d616948c59261f5e6b02376f48c8e4f277e13
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-14 13:39:28 +0100

    Add AutoMapper to Users Controller

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,31 +13,37 @@ namespace API.Controllers;
 public class UsersController : BaseApiController {
     
     private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
     
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
         var users = await _userRepository.GetUsersAsync();
-        return Ok(users);
+        var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+        return Ok(usersToReturn);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(Guid id)
+    public async Task<ActionResult<MemberDto>> GetUser(Guid id)
     {
         var user = await _userRepository.GetUserByIdAsync(id);
-        return Ok(user);
+        var userToReturn = _mapper.Map<MemberDto>(user);
+        return Ok(userToReturn);
     }
     
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         var user = await _userRepository.GetUserByUserNameAsync(username);
-        return Ok(user);
+        var userToReturn = _mapper.Map<MemberDto>(user);
+        return Ok(userToReturn);
     }
     
 }

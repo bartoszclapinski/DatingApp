2c8c3922603aba24462c15ffad7c8734d409aa2e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-14 14:13:33 +0100

    AutoMapper queryable extension

--- api/Interfaces/IUserRepository.cs
+++ api/Interfaces/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using API.Entities;
+﻿using API.DTOs;
+using API.Entities;
 
 namespace API.Interfaces;
 
@@ -9,4 +10,6 @@ public interface IUserRepository
     Task<IEnumerable<AppUser>> GetUsersAsync();
     Task<AppUser> GetUserByIdAsync(Guid id);
     Task<AppUser> GetUserByUserNameAsync(string userName);
+    Task<IEnumerable<MemberDto>> GetMembersAsync();
+    Task<MemberDto> GetMemberAsync(string userName);
 }

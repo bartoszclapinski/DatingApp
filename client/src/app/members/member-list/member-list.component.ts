e4d5254c75ffdeb82c0fb2f4aa578a9de62e86d2
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-16 15:59:56 +0100

    Getting members from server

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Member} from "../../_models/member";
+import {MembersService} from "../../_services/members.service";
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  members: Member[] = [];
 
+  constructor(private memberService: MembersService) {}
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(members => {
+      this.members = members;
+    })
+  }
 }

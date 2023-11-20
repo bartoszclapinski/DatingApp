56931815446772648f3b52f8d2a8b04f4324bdea
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-20 14:06:19 +0100

    Styling member details page

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 import {ToastrModule} from "ngx-toastr";
+import {TabsModule} from "ngx-bootstrap/tabs";
 
 
 
@@ -10,13 +11,15 @@ import {ToastrModule} from "ngx-toastr";
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right"
     })
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }

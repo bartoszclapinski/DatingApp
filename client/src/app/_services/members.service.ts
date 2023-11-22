952d181c00e847d55e64ad0d40e70f78b47d481b
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-22 16:04:43 +0100

    Using service to store state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Member} from "../_models/member";
+import {map, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,30 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   usersUrl = this.baseUrl + "users";
   userUrl = this.usersUrl + "/";
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.usersUrl);
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.usersUrl).pipe(
+      map(members => { this.members = members; return members; })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(this.userUrl + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.usersUrl, member);
+    return this.http.put(this.usersUrl, member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member};
+      })
+    );
   }
 
 }

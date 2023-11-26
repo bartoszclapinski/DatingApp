5d6b5c141f325092d98035fb55033e3a2122d402
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-11-26 18:25:44 +0100

    Photo uploader added

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,12 +1,60 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Member} from "../../_models/member";
+import {FileUploader} from "ng2-file-upload";
+import {environment} from "../../../environments/environment";
+import {User} from "../../_models/user";
+import {AccountService} from "../../_services/account.service";
+import {take} from "rxjs";
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
   styleUrls: ['./photo-editor.component.css']
 })
-export class PhotoEditorComponent {
+export class PhotoEditorComponent implements OnInit {
   @Input() member: Member | undefined;
+  uploader: FileUploader | undefined;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User | undefined;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if (user) this.user = user;
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(event: any) {
+    this.hasBaseDropZoneOver = event;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + "users/add-photo",
+      authToken: "Bearer " + this.user?.token,
+      isHTML5: true,
+      allowedFileType: ["image"],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024 // 10 MB
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member?.photos.push(photo);
+      }
+    }
+  }
 
 }

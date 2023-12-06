8c8f6129e4c273b40216a877185cb7bf661c76ad
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-06 19:42:59 +0100

    Addictional filters added

--- api/Data/UserRepository.cs
+++ api/Data/UserRepository.cs
@@ -51,8 +51,14 @@ public class UserRepository : IUserRepository
     public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
         var query = _context.Users.AsQueryable();
+        
         query = query.Where(u => u.UserName != userParams.CurrentUsername);
         query = query.Where(u => u.Gender == userParams.Gender);
+        
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+        
+        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
         
         return await PagedList<MemberDto>.CreateAsync(
                         query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), 
--- api/Helpers/UserParams.cs
+++ api/Helpers/UserParams.cs
@@ -13,4 +13,6 @@ public class UserParams
     
     public string CurrentUsername { get; set; }
     public string Gender { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 150;
 }

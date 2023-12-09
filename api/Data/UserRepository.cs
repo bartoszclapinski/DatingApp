425a9190bf49617b6ec327038031ec06591f3fd7
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-09 18:05:06 +0100

    Sort by lastActive and created added

--- api/Data/UserRepository.cs
+++ api/Data/UserRepository.cs
@@ -60,6 +60,12 @@ public class UserRepository : IUserRepository
         
         query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
         
+        query = userParams.OrderBy switch
+        {
+            "created" => query.OrderByDescending(u => u.Created),
+            _ => query.OrderByDescending(u => u.LastActive)
+        };
+        
         return await PagedList<MemberDto>.CreateAsync(
                         query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), 
                         userParams.PageNumber, 

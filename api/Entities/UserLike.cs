a595ebc882084eb57a85d6108f0e1a83793a314d
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-11 19:03:22 +0100

    Like entity added

---
+++ api/Entities/UserLike.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class UserLike
+{
+    public AppUser SourceUser { get; set; }
+    public Guid SourceUserId { get; set; }
+    public AppUser LikedUser { get; set; }
+    public Guid LikedUserId { get; set; }    
+}
---
+++ api/Migrations/20231211175743_LikeEntityAdded.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class LikeEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    SourceUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LikedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => new { x.SourceUserId, x.LikedUserId });
+                    table.ForeignKey(
+                        name: "FK_Likes_Users_LikedUserId",
+                        column: x => x.LikedUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Likes_Users_SourceUserId",
+                        column: x => x.SourceUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_LikedUserId",
+                table: "Likes",
+                column: "LikedUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Likes");
+        }
+    }
+}

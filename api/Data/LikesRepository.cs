3d741ad18db80802ef2591ad4ce588973e770b08
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-11 19:13:13 +0100

    Likes repository added

---
+++ api/Data/LikesRepository.cs
@@ -0,0 +1,23 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Data;
+
+public class LikesRepository : ILikesRepository
+{
+    public Task<UserLike> GetUserLike(Guid sourceUserId, Guid likedUserId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AppUser> GetUserWithLikes(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}

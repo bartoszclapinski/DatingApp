256a25f5ee169827ccbded230367c556e4cdae26
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-12 17:33:45 +0100

    Message entity added

---
+++ api/Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Entities;
+
+public class Message
+{
+    public Guid Id { get; set; }
+    public Guid SenderId { get; set; }
+    public string SenderUsername { get; set; } = null!;
+    public AppUser Sender { get; set; } = null!;
+    public Guid RecipientId { get; set; }
+    public string RecipientUsername { get; set; } = null!;
+    public AppUser Recipient { get; set; } = null!;
+    public string Content { get; set; } = null!;
+    public DateTime? DateRead { get; set; }
+    public DateTime MessageSent { get; set; } = DateTime.UtcNow;
+    public bool SenderDeleted { get; set; }
+    public bool RecipientDeleted { get; set; }
+}

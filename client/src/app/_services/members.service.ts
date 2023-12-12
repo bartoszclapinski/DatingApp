c4b2853564b5139867d9e6bada59db74d4977e8a
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-12 12:59:26 +0100

    Lists of likes update part 1

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -98,7 +98,7 @@ export class MembersService {
   }
 
   getLikes(predicate: string) {
-    return this.http.get(this.baseUrl + "likes?predicate=" + predicate);
+    return this.http.get<Member[]>(this.baseUrl + "likes?predicate=" + predicate);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Member} from "../_models/member";
+import {MembersService} from "../_services/members.service";
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+  members: Member[] | undefined;
+  predicate: string = "liked";
+
+  constructor(private memberService: MembersService) {
+  }
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe({
+      next: response => {
+        this.members = response;
+      }
+    });
+  }
+
 
 }

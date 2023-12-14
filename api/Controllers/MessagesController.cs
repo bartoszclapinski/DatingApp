5a5f32ac071cee914771986576abe6f17c639eb0
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-14 19:14:52 +0100

    Message controller added

---
+++ api/Controllers/MessagesController.cs
@@ -0,0 +1,53 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authorize]
+public class MessagesController : BaseApiController
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMessageRepository _messageRepository;
+    private readonly IMapper _mapper;
+
+    public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, IMapper mapper)
+    {
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _messageRepository = messageRepository ?? throw new ArgumentNullException(nameof(messageRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateMessage([FromBody] CreateMessageDto createMessageDto)
+    {
+        var username = User.GetUsername();
+        if (username == createMessageDto.RecipientUsername.ToLower())
+            return BadRequest("You cannot send messages to yourself");
+        
+        var sender = await _userRepository.GetUserByUserNameAsync(username);
+        var recipient = await _userRepository.GetUserByUserNameAsync(createMessageDto.RecipientUsername);
+        
+        if (recipient == null) return NotFound();
+        
+        var message = new Message
+        {
+            Id = new Guid(),
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+        
+        _messageRepository.AddMessage(message);
+        
+        if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+        
+        return BadRequest("Failed to send message");
+    }
+}
--- api/DTOs/MessageDto.cs
+++ api/DTOs/MessageDto.cs
@@ -4,11 +4,11 @@ public class MessageDto
 {
     public Guid Id { get; set; }
     public Guid SenderId { get; set; }
-    public string SenderUsername { get; set; } = null!;
-    public string SenderPhotoUrl { get; set; } = null!;
+    public string SenderUsername { get; set; } 
+    public string SenderPhotoUrl { get; set; }
     public Guid RecipientId { get; set; }
-    public string RecipientUsername { get; set; } = null!;
-    public string RecipientPhotoUrl { get; set; } = null!;
+    public string RecipientUsername { get; set; }
+    public string RecipientPhotoUrl { get; set; }
     public string Content { get; set; } = null!;
     public DateTime? DateRead { get; set; }
     public DateTime MessageSent { get; set; }

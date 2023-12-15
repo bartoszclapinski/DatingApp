a54270b45d753c2f8cd6f09322040c9bb81de979
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-15 16:12:41 +0100

    Get message from database added

--- api/Controllers/MessagesController.cs
+++ api/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -49,5 +50,18 @@ public class MessagesController : BaseApiController
         if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
         
         return BadRequest("Failed to send message");
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+    {
+        messageParams.UserName = User.GetUsername();
+        
+        var messages = await _messageRepository.GetMessagesForUser(messageParams);
+        
+        Response.AddPaginationsHeader(
+            new PaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages));
+        
+        return messages;
     }
 }
--- api/Interfaces/IMessageRepository.cs
+++ api/Interfaces/IMessageRepository.cs
@@ -9,7 +9,7 @@ public interface IMessageRepository
     void AddMessage(Message message);
     void DeleteMessage(Message message);
     Task<Message> GetMessage(Guid id);
-    Task<PagedList<MessageDto>> GetMessagesForUser();
+    Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);  
     Task<IEnumerable<MessageDto>> GetMessageThread(Guid currentUserId, Guid recipientId);
     Task<bool> SaveAllAsync();
 }

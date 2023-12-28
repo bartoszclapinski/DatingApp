4ac6b89f00b9b0c619ac77493c93543368c6ba4c
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-12-28 14:57:16 +0100

    Send message setup on client

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,22 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from "../../_modules/message";
 import {MessageService} from "../../_services/message.service";
 import {CommonModule} from "@angular/common";
 import {TimeagoModule} from "ngx-timeago";
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
   templateUrl: './member-messages.component.html',
-  imports: [CommonModule, TimeagoModule],
+  imports: [CommonModule, TimeagoModule, FormsModule],
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit{
+  @ViewChild('messageForm', {static: true}) messageForm?: NgForm;
   @Input() messages: Message[] = [];
   @Input() username: string = "";
-  constructor() { }
+  messageContent: string = "";
+  constructor(private messageService: MessageService) { }
 
   ngOnInit() {
+  }
+
+  sendMessage() {
+    if (!this.username) return;
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages.push(message);
+        this.messageForm?.reset();
+      }
+    });
   }
 
 

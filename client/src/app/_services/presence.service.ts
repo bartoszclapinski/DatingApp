cdbd560af650b669ddb5751987fafb1db37f3908
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-01-18 13:11:08 +0100

    Display online presence

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../environments/environment";
 import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
 import {ToastrService} from "ngx-toastr";
 import {User} from "../_models/user";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ import {User} from "../_models/user";
 export class PresenceService {
   hubUrl = environment.hubUrl;
   private hubConnection?: HubConnection;
+  private onlineUsersSource = new BehaviorSubject<string[]>([]);
+  onlineUsers$ = this.onlineUsersSource.asObservable();
 
   constructor(private toastr: ToastrService) { }
 
@@ -27,6 +30,10 @@ export class PresenceService {
 
     this.hubConnection.on("UserIsOffline", username => {
       this.toastr.warning(username + " has disconnected");
+    });
+
+    this.hubConnection.on("GetOnlineUsers", (usernames: string[]) => {
+      this.onlineUsersSource.next(usernames);
     });
   }
 

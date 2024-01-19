05397449ad2449b5a891ab8c865e0c7f5a8057e2
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-01-19 09:21:37 +0100

    Messgae hub added

---
+++ api/SignalR/MessageHub.cs
@@ -0,0 +1,40 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR;
+
+public class MessageHub : Hub
+{
+    private readonly IMessageRepository _messageRepository;
+
+    public MessageHub(IMessageRepository messageRepository)
+    {
+        _messageRepository = messageRepository ?? throw new ArgumentNullException(nameof(messageRepository));    
+    }
+    
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        if (httpContext != null)
+        {
+            var otherUser = httpContext.Request.Query["user"].ToString();
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        
+            var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+    }
+    
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    private string GetGroupName(string caller, string other)
+    {
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}

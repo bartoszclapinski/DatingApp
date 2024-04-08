b452936f78df5ac1d8b3e8f1dc45861e1ffb2d07
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-04-08 14:22:50 +0200

    Confirm service added

---
+++ client/src/app/_services/confirm.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {ConfirmDialogComponent} from "../modals/confirm-dialog/confirm-dialog.component";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmService {
+  bsModalRef?: BsModalRef<ConfirmDialogComponent>;
+
+  constructor(private modalService: BsModalService) { }
+
+  confirm(
+    title = 'Confirmation',
+    message = 'Are you sure you want to do this?',
+    btnOkText = 'Ok',
+    btnCancelText = 'Cancel'
+  ) {
+    const config = {
+      initialState: {
+        title,
+        message,
+        btnOkText,
+        btnCancelText
+      }
+    }
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+  }
+}
---
+++ client/src/app/modals/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import { BsModalRef } from "ngx-bootstrap/modal";
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent implements OnInit{
+  title: string = '';
+  message: string = '';
+  btnOkText: string = '';
+  btnCancelText: string = '';
+  result: boolean = false;
+
+  constructor(public bsModalRef: BsModalRef) { }
+
+  ngOnInit(): void {
+  }
+
+  confirm() {
+    this.result = true;
+    this.bsModalRef.hide();
+  }
+
+  decline() {
+    this.bsModalRef.hide();
+  }
+}

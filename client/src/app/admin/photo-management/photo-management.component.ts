1e39f4cefc73beafc0487742c6f6a3c06c6ae65a
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-04-08 19:47:06 +0200

    Photo approve added

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Photo} from "../../_models/photo";
+import {AdminService} from "../../_services/admin.service";
 
 @Component({
   selector: 'app-photo-management',
   templateUrl: './photo-management.component.html',
   styleUrls: ['./photo-management.component.css']
 })
-export class PhotoManagementComponent {
+export class PhotoManagementComponent implements OnInit {
+  photos: Photo[] = [];
+
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit(): void {
+    this.getPhotosForApproval();
+  }
+
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe(photos => this.photos = photos);
+  }
+
+  approvePhoto(photoId: any) {
+    this.adminService.approvePhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    });
+  }
+
+  rejectPhoto(photoId: any) {
+    this.adminService.rejectPhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    });
+  }
 
 }
